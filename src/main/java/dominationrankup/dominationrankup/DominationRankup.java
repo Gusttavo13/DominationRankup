34e506913d6eed96604ee1a74a811b0493ae9fb7
Gusttavo <gustavosousa918@gmail.com>
2022-11-18 15:56:55 -0300

    init commit

--- src/main/java/dominationrankup/dominationrankup/DominationRankup.java
+++ src/main/java/dominationrankup/dominationrankup/DominationRankup.java
@@ -1,17 +1,17 @@
 package dominationrankup.dominationrankup;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class DominationRankup extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        Bukkit.getConsoleSender().sendMessage("§e[DominationRankup] §aIniciando.");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        Bukkit.getConsoleSender().sendMessage("§e[DominationRankup] §aDesligando.");
     }
 }
